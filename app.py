0cfbad28017d32f71fda36491e6ed3497ae80f10
dkyxhjj <richardli.060411@gmail.com>
2025-07-30 18:19:59 -0400

    changed port

--- app.py
+++ app.py
@@ -554,5 +554,5 @@ def add_credits():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 4999))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
